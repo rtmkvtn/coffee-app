835c610df3f159eb7c5500efa7098f65f97fd90b
Artem Kovtun <rtmkvtn@gmail.com>
2025-05-12 12:45:50 +0800

    feat: add status icons to order list items

--- src/views/ordersPage/orderListItem/OrderListItem.tsx
+++ src/views/ordersPage/orderListItem/OrderListItem.tsx
@@ -2,6 +2,8 @@ import { formatDate, formatPrice, getOrderStatusText } from '@lib/helpers'
 import { IOrder } from '@models/index'
 import classNames from 'classnames'
 
+import Icon from '@assets/images/Icon'
+
 import styles from './OrderListItem.module.scss'
 
 type OrderListItemProps = {
@@ -29,11 +31,30 @@ const OrderListItem = ({ order, className }: OrderListItemProps) => {
     }
   }
 
+  const getStatusIcon = (state: IOrder['state']) => {
+    switch (state) {
+      case 'waitingForPayment':
+        return <Icon type="collectCoins" size={16} />
+      case 'paymentProcessing':
+      case 'paid':
+        return <Icon type="slip" size={16} />
+      case 'preparing':
+        return <Icon type="clock" size={16} />
+      case 'completed':
+        return <Icon type="check" size={16} />
+      case 'canceled':
+        return <Icon type="close" size={16} />
+      default:
+        return null
+    }
+  }
+
   return (
     <div className={classNames(styles.wrapper, className)}>
       <div className={styles.header}>
         <div className={styles.id}># {order.id}</div>
         <div className={classNames(styles.status, getStatusColor(order.state))}>
+          {getStatusIcon(order.state)}
           {getOrderStatusText(order.state)}
         </div>
       </div>
